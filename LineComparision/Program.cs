a2ba643608d4e3e346e62a33342bdfd1ebdb4c54
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-11 20:21:46 +0530

    small and irrelevant modification

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -11,9 +11,9 @@ namespace LineComparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Project");
-            int x1,y1, x2, y2;
+            float x1,y1, x2, y2;
             float distance;
-
+            //UseCase 1
             Console.WriteLine("Enter the point x1");
             x1=Convert.ToInt32(Console.ReadLine());
             
@@ -27,6 +27,25 @@ namespace LineComparision
             y2=Convert.ToInt32(Console.ReadLine());
            distance = (float)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
             Console.WriteLine("Distance between two points({0},{1}),({2},{3}) is :{4}", x1,y1,x2,y2,distance);
+            //UseCase 2
+            Console.WriteLine("enter one more point coordinates to find whether the given two lines are equal or not");
+
+            Console.WriteLine("enter the point z1");
+            float z1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the point z2");
+            float z2 = Convert.ToInt32(Console.ReadLine());
+
+            if ((x1 / x2 == y1 / y2)
+                  && (x1 / x2 == z1 / z2)
+                    && (y1 / y2 == z1 / z2))
+                Console.WriteLine("The given straight"
+                + " lines are identical");
+
+            else
+                Console.WriteLine("The given straight"
+                    + " lines are not identical");
+                    
             Console.ReadLine();
         }
     }
